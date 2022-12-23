1844ecb1100baefd6b719afec6c7e5b98c8f3919
Johnson Ojo <johnsonojo89@gmail.com>
2022-12-23 08:52:55 +0100

    update resume link

--- src/constants/homeData.js
+++ src/constants/homeData.js
@@ -1,5 +1,5 @@
 export const homeData = {
   aboutMe: ["Software Engineer", "Technical Writer", "Blockchain Enthusiast"],
   resumeLink:
-    "https://drive.google.com/file/d/1mDsUHqy1xJEgcKnHzBjOTwRBS0UDpLYx/view?usp=share_link",
+    "https://drive.google.com/file/d/1eLxEuQe2XzUYc6LuQfdexGKbDC5u9oHO/view?usp=share_link",
 };
